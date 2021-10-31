12af820d9ed1f9905f17cbe17fb3497930159302
infobarbosa <infobarbosa@yahoo.com.br>
2021-10-31 11:22:08 -0300

    adicionando outros métodos de serviço.

--- src/main/java/com/github/infobarbosa/bankaccount/customer/CustomerService.java
+++ src/main/java/com/github/infobarbosa/bankaccount/customer/CustomerService.java
@@ -1,8 +1,14 @@
 package com.github.infobarbosa.bankaccount.customer;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomerService {
     
+
+    public Customer create(Customer customer);
+    public Customer update(Customer customer);
+
+    public Optional<Customer> get(Long id);
     public List<Customer> getAll();
 }
--- src/main/java/com/github/infobarbosa/bankaccount/customer/CustomerServiceImpl.java
+++ src/main/java/com/github/infobarbosa/bankaccount/customer/CustomerServiceImpl.java
@@ -1,16 +1,37 @@
 package com.github.infobarbosa.bankaccount.customer;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService{
+    private final Logger logger = LoggerFactory.getLogger( CustomerServiceImpl.class.getName() );
+
     @Autowired
     private CustomerRepository customerRepository;
 
     public List<Customer> getAll(){
         return (List<Customer>) customerRepository.findAll();
+    }
+
+    public Optional<Customer> get(Long id){
+        return customerRepository.findById( id );
+    }
+
+    public Customer create(Customer customer) {
+        Customer created = customerRepository.save( customer );
+        logger.debug( created.toString() );
+        return created;
+    }
+
+    public Customer update(Customer customer){
+        logger.debug( "Atualizando customer: " + customer.toString() );
+        Customer updated = customerRepository.save(customer);
+        return updated;
     }
 }
