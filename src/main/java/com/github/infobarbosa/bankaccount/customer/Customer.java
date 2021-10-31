366acc950cda02b8f862de7047690b835833185c
infobarbosa <infobarbosa@yahoo.com.br>
2021-10-31 18:57:17 -0300

    adicionando definições da coluna NAME

--- src/main/java/com/github/infobarbosa/bankaccount/customer/Customer.java
+++ src/main/java/com/github/infobarbosa/bankaccount/customer/Customer.java
@@ -2,6 +2,7 @@ package com.github.infobarbosa.bankaccount.customer;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,6 +16,8 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length=100, nullable=false)
     private String name;
 
     public Customer(){}
