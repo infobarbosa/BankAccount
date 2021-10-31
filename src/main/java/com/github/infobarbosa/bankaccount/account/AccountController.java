2d506c43276e2ea8c911f4f8151e00c2fa93e8ec
infobarbosa <infobarbosa@yahoo.com.br>
2021-10-30 22:58:24 -0300

    Utilizando @GetMapping ao invés de @RequestMapping. Colocando o path no plural (naming conventions)

--- src/main/java/com/github/infobarbosa/bankaccount/account/AccountController.java
+++ src/main/java/com/github/infobarbosa/bankaccount/account/AccountController.java
@@ -4,19 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/account")
+@RestController
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
 
-    @RequestMapping(value = "/account", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Account> getAllAccounts(){
         return accountService.findAll();
     }
