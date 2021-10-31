78acd18e75d1185496b8e0fe18f62700bfb52255
infobarbosa <infobarbosa@yahoo.com.br>
2021-10-31 11:15:00 -0300

    adicionando outros métodos HTTP. melhorando os retornos dos métodos.

--- src/main/java/com/github/infobarbosa/bankaccount/customer/CustomerController.java
+++ src/main/java/com/github/infobarbosa/bankaccount/customer/CustomerController.java
@@ -1,11 +1,16 @@
 package com.github.infobarbosa.bankaccount.customer;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +23,22 @@ public class CustomerController {
     @GetMapping(value = PATH, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Customer> getAll(){
         return customerService.getAll();
+    }
+
+    @GetMapping(value = PATH + "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Optional<Customer> get(@PathVariable Long id){
+        return customerService.get( id );
+    }
+
+    @PostMapping(value = PATH)
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Customer create(Customer customer){
+        return customerService.create( customer );
+    }
+
+    @PutMapping(value = PATH)
+    @ResponseStatus(code = HttpStatus.OK)
+    public Customer update(Customer customer){
+        return customerService.update( customer );
     }
 }
